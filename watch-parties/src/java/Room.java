affc6a46c77d30cc36f83dd0e6a229089cd212f0
Garrett Landen <glanden@uvm.edu>
2020-06-30 19:13:26 +0000

    Add Room Class

--- watch-parties/src/java/Room.java
+++ watch-parties/src/java/Room.java
@@ -4,12 +4,34 @@ import java.lang.Integer;
 public class Room {
   private String title;
   private int partyLeaderID;
+  private String videoURL;
   private ArrayList<Integer> currentUserIDs;
   private boolean privateRoom;
-  private int currentVideo;//index of current video in queue
+  //private int currentVideo;//index of current video in queue
   private boolean paused;
   private int currentPosition;
-  private ArrayList<Video> queuedVideos;
-  private ArrayList<Video> suggestedVideos;
+  //private ArrayList<Video> queuedVideos;
+  //private ArrayList<Video> suggestedVideos;
+  public void createRoomEntity(){
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    Entity entity = new Entity("Room");
+    taskEntity.setProperty("title", title);
+    taskEntity.setProperty("users", currentUserIDs);
+    taskEntity.setProperty("privateRoom", privateRoom);
+    taskEntity.setProperty("paused", paused);
+    taskEntity.setProperty("currentPosition", currentPosition);
+    taskEntity.setProperty("partyLeaderID", partyLeaderID);
+    taskEntity.setProperty("videoURL", videoURL);
+    datastore.put(entity);
+  }
+  public Room(Entity entity){
+    title = entity.getProperty("title");
+    partyLeaderID = entity.getProperty("partyLeaderID");
+    currentUserIDs = entity.getProperty("users");
+    privateRoom = entity.getProperty("privateRoom");
+    paused = entity.getProperty("paused");
+    currentPosition = entity.getProperty("currentPosition");
+    videoURL = entity.getProperty("videoURL");
+  }
 }
 
