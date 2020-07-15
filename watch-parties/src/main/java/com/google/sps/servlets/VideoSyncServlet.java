4e607b06f5e108b7ce35b068c1919a77e5e772e1
Sean Dohring <seandohring@gmail.com>
2020-07-15 13:45:23 -0700

    Bad change to video sync servlet

--- watch-parties/src/main/java/com/google/sps/servlets/VideoSyncServlet.java
+++ watch-parties/src/main/java/com/google/sps/servlets/VideoSyncServlet.java
@@ -33,7 +33,7 @@ public class VideoSyncServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException { 
         response.setContentType("application/json");
-        response.getWriter().println("{\"status\": \"" + videoStatus + "\"}");
+        response.getWriter().println("{\"status\": \"" + 500 + "\"}");
     }
 
     @Override
