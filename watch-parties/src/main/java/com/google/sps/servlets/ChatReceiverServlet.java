b5e54ff02f6d29b97a57da9802e270924cbc673f
Akin Mason-Hogans <akinlabimh@gmail.com>
2020-08-06 17:15:44 +0000

    save

--- watch-parties/src/main/java/com/google/sps/servlets/ChatReceiverServlet.java
+++ watch-parties/src/main/java/com/google/sps/servlets/ChatReceiverServlet.java
@@ -23,18 +23,29 @@ import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.sps.data.Chat;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
  
 @WebServlet("/chatstorer")
 public class ChatReceiverServlet extends HttpServlet {
-    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
  
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String comment = request.getParameter("submitted-comment");
         long timestamp = System.currentTimeMillis();
         String authorID = "User"; //placeholder for now
-
-        // Add Author ID stuff here
+        
+        Query query = new Query("Member");
+        UserService userService = UserServiceFactory.getUserService();
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        PreparedQuery results = datastore.prepare(query);
+        for (Entity entity : results.asIterable()) {
+            if(entity.getProperty("user").equals(userService.getCurrentUser()))
+            authorID=entity.getProperty("nickname").toString();
+        } 
 
         // From here on we make sure the message is stored properly
         Entity chatEntity = new Entity("Chat");
@@ -43,7 +54,6 @@ public class ChatReceiverServlet extends HttpServlet {
         chatEntity.setProperty("timestamp", timestamp);
 
         // Store chat message
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
         datastore.put(chatEntity);
         response.setStatus(HttpServletResponse.SC_OK); 
     }    
